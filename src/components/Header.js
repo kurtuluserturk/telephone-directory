fd8288f54eb906cad947f69edf050b2a38e70e34
Kurtuluş ERTÜRK <47227072+kurtuluserturk@users.noreply.github.com>
2021-05-12 18:48:14 +0300

    Header.js created

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div className="text-center">
+            <h1>Telephone Directory App</h1>
+        </div>
+    )
+}
+
+export default Header
