d34355c8057314704d5f27c70431da396a8e5afc
Kurtuluş ERTÜRK <47227072+kurtuluserturk@users.noreply.github.com>
2021-05-12 18:50:11 +0300

    login page created

---
+++ src/pages/login.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import { Button, Form } from 'react-bootstrap'
+import { useGlobalContext } from '../context';
+
+const Login = () => {
+    const {
+        admin,
+        email,
+        password,
+        saveEmail,
+        savePassword,
+        handleLoginSubmit
+    } = useGlobalContext();
+
+    return (
+        <div className="items-center">
+            <Form onSubmit={handleLoginSubmit} className="text-center w-500 mt-50">
+                <Form.Group controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control
+                        type="email"
+                        value={email}
+                        onChange={(e) => { saveEmail(e) }}
+                        placeholder="Enter email"
+                        required
+                    />
+                </Form.Group>
+
+                <Form.Group controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control
+                        type="password"
+                        placeholder="Password"
+                        value={password}
+                        onChange={(e) => { savePassword(e) }}
+                        required
+                    />
+                </Form.Group>
+
+                <Button variant="primary" type="submit">
+                    Submit
+                </Button>
+            </Form>
+        </div>
+    )
+}
+
+export default Login
