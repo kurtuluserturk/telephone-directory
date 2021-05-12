1cd0e9f1af02b9fda3c1fd29c53dc2b10c40e77b
Kurtuluş ERTÜRK <47227072+kurtuluserturk@users.noreply.github.com>
2021-05-12 18:49:04 +0300

    routing structure created

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
+import { Switch, Route } from 'react-router';
 import './App.css';
+import Header from './components/Header'
+import Login from './pages/login';
+import PeopleList from './pages/peopleList'
+import Registration from './pages/registration';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={Login} />
+        <Route path="/people" component={PeopleList} />
+        <Route path="/registration" component={Registration} />
+      </Switch>
+    </>
   );
 }
 
