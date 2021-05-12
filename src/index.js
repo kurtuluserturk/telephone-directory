372d851f5c9df68619b909e54331fc5325a5b81f
Kurtuluş ERTÜRK <47227072+kurtuluserturk@users.noreply.github.com>
2021-05-12 18:47:41 +0300

    App wrapped with router and provider

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
+import { BrowserRouter as Router } from "react-router-dom";
+import { UserProvider } from './context'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <UserProvider>
+      <Router>
+        <App />
+      </Router>
+    </UserProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
