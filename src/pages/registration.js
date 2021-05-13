b0b860e09f8b47d12caa14e84603bde06724846e
Kurtuluş ERTÜRK <47227072+kurtuluserturk@users.noreply.github.com>
2021-05-13 21:35:26 +0300

    Registration updated

--- src/pages/registration.js
+++ src/pages/registration.js
@@ -5,77 +5,71 @@ import { useGlobalContext } from '../context';
 
 const Registration = () => {
     const {
-        firstName,
-        lastName,
+        admin,
+        fullName,
         email,
         phone,
-        saveFirstName,
-        saveLastName,
+        saveFullName,
         saveEmail,
         savePhone,
         handleRegistrationSubmit
     } = useGlobalContext();
 
     return (
-        <>
-            <div className="items-center">
-                <Form onSubmit={handleRegistrationSubmit} className="text-center w-500 mt-50">
+        <div>
+            {
+                admin
+                    ? <div className="items-center">
+                        <div className="text-center w-500 mt-50">
+                            <Form onSubmit={handleRegistrationSubmit}>
 
-                    <Form.Group controlId="formBasicFirstName">
-                        <Form.Label>FirstName</Form.Label>
-                        <Form.Control
-                            type="text"
-                            placeholder="FirstName"
-                            value={firstName}
-                            onChange={(e) => { saveFirstName(e) }}
-                            required
-                        />
-                    </Form.Group>
+                                <Form.Group controlId="formBasicFirstName">
+                                    <Form.Label>Full Name</Form.Label>
+                                    <Form.Control
+                                        type="text"
+                                        placeholder="Full Name"
+                                        value={fullName}
+                                        onChange={(e) => { saveFullName(e) }}
+                                        required
+                                    />
+                                </Form.Group>
 
-                    <Form.Group controlId="formBasicLastName">
-                        <Form.Label>LastName</Form.Label>
-                        <Form.Control
-                            type="text"
-                            placeholder="LastName"
-                            value={lastName}
-                            onChange={(e) => { saveLastName(e) }}
-                            required
-                        />
-                    </Form.Group>
+                                <Form.Group controlId="formBasicPhone">
+                                    <Form.Label>Phone</Form.Label>
+                                    <Form.Control
+                                        type="tel"
+                                        placeholder="05XX-XXXXXX"
+                                        value={phone}
+                                        onChange={(e) => { savePhone(e) }}
+                                        required
+                                    />
+                                </Form.Group>
 
-                    <Form.Group controlId="formBasicPhone">
-                        <Form.Label>Phone</Form.Label>
-                        <Form.Control
-                            type="tel"
-                            placeholder="05XX-XXXXXX"
-                            value={phone}
-                            onChange={(e) => { savePhone(e) }}
-                            required
-                        />
-                    </Form.Group>
+                                <Form.Group controlId="formBasicEmail">
+                                    <Form.Label>Email address</Form.Label>
+                                    <Form.Control
+                                        type="email"
+                                        value={email}
+                                        onChange={(e) => { saveEmail(e) }}
+                                        placeholder="Enter email"
+                                        required
+                                    />
+                                </Form.Group>
 
-                    <Form.Group controlId="formBasicEmail">
-                        <Form.Label>Email address</Form.Label>
-                        <Form.Control
-                            type="email"
-                            value={email}
-                            onChange={(e) => { saveEmail(e) }}
-                            placeholder="Enter email"
-                            required
-                        />
-                    </Form.Group>
+                                <Button variant="primary" type="submit">
+                                    Save
+                                </Button>
 
-                    <Button variant="primary" type="submit">
-                        Save
-                </Button>
-                </Form>
+                            </Form>
 
-
-            </div>
-            <Link to="/people" className="items-center mt-50">
-                People List
-            </Link>
-        </>
+                            <Link to="/people" className="items-center mt-50">
+                                People List
+                            </Link>
+                        </div>
+                    </div>
+                    : <h4 className="items-center">You have to login to see this page</h4>
+            }
+        </div>
     )
 }
 
